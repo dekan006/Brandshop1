3b970dc9be2164f131997a74e69498f18e19683b
Denis Kanash <70863033+dekan006@users.noreply.github.com>
2021-06-17 03:26:19 +0300

    Add files via upload

---
+++ script/all.js
@@ -0,0 +1,39 @@
+jQuery(function($) {
+
+    console.log("jQuery start");
+
+$(document).ready(function(){
+    $('.brand_container').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 1
+            }
+        }]
+    });
+
+    $('.banner_2_bg').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+	
+	
+});
+	
+})
